6feae0279afbc70f6bc1aa073247e4c7f290bc18
HUANG Bocheng <a923000213@163.com>
2020-02-17 18:58:17 -0700

    Update cFrameBuffer.cpp

--- OpenGLStudy/Engine/Graphics/FrameBuffer/cFrameBuffer.cpp
+++ OpenGLStudy/Engine/Graphics/FrameBuffer/cFrameBuffer.cpp
@@ -14,7 +14,7 @@ namespace Graphics {
 
 		// Generate another frame buffer
 		glGenFramebuffers(1, &m_fbo);
-		std::string key = "FB_" +std::to_string(m_fbo) + "_ETT_" + std::to_string(i_textureType);
+		std::string key = "FB_" + std::to_string(m_fbo) + "_ETT_" + std::to_string(i_textureType);
 
 		// Generate render_to_texture texture
 		cTexture::s_manager.Load(key, m_renderToTexture, i_textureType, m_width, m_height);
@@ -24,37 +24,23 @@ namespace Graphics {
 			// bind the frame buffer, it can be read / draw. GL_DRAW_FRAMEBUFFER / GL_READ_FRAMEBUFFER
 			glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
 
-			auto _glAttachment = 0;
 			switch (i_textureType)
 			{
 			case Graphics::ETT_FRAMEBUFFER_SHADOWMAP:
-				_glAttachment = GL_DEPTH_ATTACHMENT;
-				break;
-			case Graphics::ETT_FRAMEBUFFER_COLOR:
-				_glAttachment = GL_COLOR_ATTACHMENT0;
-				break;
-			default:
-				result = false;
-				printf("Invalid texture type in creating frame buffer.\n");
-				return result;
-				break;
-			}
-			// Ref: https://open.gl/framebuffers
-			// bind the frame buffer to a texture
-			// The second parameter implies that you can have multiple color attachments. 
-			//A fragment shader can output different data to any of these by linking out variables to attachments with the glBindFragDataLocation function
-			glFramebufferTexture2D(GL_FRAMEBUFFER, _glAttachment, GL_TEXTURE_2D, _texture->GetTextureID(), mipMapLevel);
-
-			// no need to draw color values if it is a depth attachment
-			if (_glAttachment == GL_DEPTH_ATTACHMENT) {
+				// Ref: https://open.gl/framebuffers
+				// bind the frame buffer to a texture
+				// The second parameter implies that you can have multiple color attachments. 
+				//A fragment shader can output different data to any of these by linking out variables to attachments with the glBindFragDataLocation function
+				glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, _texture->GetTextureID(), mipMapLevel);
 				glDrawBuffer(GL_NONE);
 				glReadBuffer(GL_NONE);
-			}
-			else if (_glAttachment == GL_COLOR_ATTACHMENT0) {
+				break;
+			case Graphics::ETT_FRAMEBUFFER_COLOR:
+
 				glDrawBuffer(GL_COLOR_ATTACHMENT0);
 
 				// We need depth too!
-				// Use render buffer with frame buffer such that we can have a depth and color at the same time
+				// Use create depth&stencil render buffer in the frame buffer such that we can have a depth and color at the same time
 				{
 					GLuint rboDepthStencil;
 					glGenRenderbuffers(1, &rboDepthStencil);
@@ -64,6 +50,12 @@ namespace Graphics {
 						GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rboDepthStencil
 					);
 				}
+				break;
+			default:
+				result = false;
+				printf("Invalid texture type in creating frame buffer.\n");
+				return result;
+				break;
 			}
 
 			GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
@@ -81,6 +73,7 @@ namespace Graphics {
 			printf("Initialize frame buffer error, can not create frame buffer without a texture id\n");
 			result = false;
 		}
+		
 		return result;
 	}
 
@@ -117,7 +110,7 @@ namespace Graphics {
 
 	bool cFrameBuffer::IsValid() const
 	{
-		return (m_fbo!=0);
+		return (m_fbo != 0);
 	}
 
 }
